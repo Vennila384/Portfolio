9d6d3ac1db3ffc8ca5afdbe1b2eb06ad52869560
Vennila384 <vennik447@gmail.com>
2025-03-17 19:10:03 +0530

    Small change

--- src/components/OtherProject.jsx
+++ src/components/OtherProject.jsx
@@ -1,11 +1,12 @@
 
 
-export default function OtherProject({image,description,link})
+export default function OtherProject({image,description,link,name})
 {
     return(
         <div id="otherProjectDetails">
             <div id="otherProject">
                 <img src={image} id="otherProjectImage"/>
+                <div id="otherProjectName">{name}</div>
             </div>
             <a href={link} target="_blank">
                 <div id="otherProjectDescription">
--- src/components/OtherProjectDetails.jsx
+++ src/components/OtherProjectDetails.jsx
@@ -4,22 +4,22 @@ import OtherProject from "./OtherProject"
 export default function OtherProjectDetails({skill,setSkill})
 {
     const jsProject = [
-        {image:"rock_paper_scissor.png",description:"Rock Paper Scissors is a game where you choose rock, paper, or scissors to win.",link:"https://vennilavennila-6404-8443.zcodeusers.in/jsAssignment023/game.html"},
+        {image:"rock_paper_scissor.png",name:"Rock Paper Scissor",description:"Rock Paper Scissors is a game where you choose rock, paper, or scissors to win.",link:"https://vennilavennila-6404-8443.zcodeusers.in/jsAssignment023/game.html"},
         {image:"mastermind.png",name:"Mastermind",description:"Mastermind is a game where you guess a secret code using logic.",link:"https://vennilavennila-6404-8443.zcodeusers.in/jsProject/masterMind.html"},
         {image:"tipCalculator.png",name:"Tip Calculator",description:"A tip calculator helps you calculate the tip amount.",link:"https://vennilavennila-6404-8443.zcodeusers.in/jsAssignment024/tip.html"},
         {image:"calculator.png",name:"Calculator",description:"A calculator is a tool for solving math problems.",link:"https://vennilavennila-6404-8443.zcodeusers.in/jsAssignment021/calculator.html"}
     ]
 
     const reactProject = [
-        {image:"onlinemeeting.jpg",description:"An online meeting is a virtual gathering using the internet.",link:""}
+        {image:"onlinemeeting.jpg",name:"Kathaikalaam",description:"An online meeting is a virtual gathering using the internet.",link:""}
     ]
 
     const javaProject = [
-        {image:"bookStore.png",description:"An online bookstore is a website where you can buy books.",link:"https://github.com/Vennila384/OnlineBookStore"}
+        {image:"bookStore.png",name:"Online book store",description:"An online bookstore is a website where you can buy books.",link:"https://github.com/Vennila384/OnlineBookStore"}
     ]
 
     const scratchProject = [
-        {image:"quiz.png",description:"A quiz application is a fun game with many categories where you answer questions.",link:"https://scratch.mit.edu/projects/1041097324"},
+        {image:"quiz.png",description:"A quiz application is a fun game with many categories where you answer questions.",name:"Quiz",link:"https://scratch.mit.edu/projects/1041097324"},
         {image:"spiral.png",description:"Spiral drawing is an art technique that creates curved, looping patterns.",name:"Spiral Drawing",link:"https://scratch.mit.edu/projects/1030484448"},
         {image:"calendar.png",description:"A day finder is a tool that tells you the day of the week for any date.",name:"Day Finder",link:"https://scratch.mit.edu/projects/1034779853"},
         // {image:"",description:"",name:"Drawing Shapes",link:"https://scratch.mit.edu/projects/1035635393"},
@@ -27,27 +27,27 @@ export default function OtherProjectDetails({skill,setSkill})
     ]
 
     const htmlProject = [
-        {image:"breezy.png",description:"Breezy.hr is a website for hiring and managing job applications.",link:"https://vennilavennila-6404-8443.zcodeusers.in/htmlproject/main.html"},
-        {image:"vortex.jpg",description:"Vortex by Pixelarity",link:"https://vennilavennila-6404-8443.zcodeusers.in/Assignment015/page1.html"},
-        {image:"magnetic.jpg",description:"Magnetic by Pixelarity",link:"https://vennilavennila-6404-8443.zcodeusers.in/Assignment010/magnetic.html"},
-        {image:"cascade.jpg",description:"Cascade by Pixelarity",link:"https://vennilavennila-6404-8443.zcodeusers.in/Assignment018/page2.html"},
-        {image:"hyperspace.jpg",description:"Hyperspace is a sleek and modern website template designed for startups and businesses.",link:"https://vennilavennila-6404-8443.zcodeusers.in/Assignment019/page1.html"},
-        {image:"3d.png",description:"A 3D rotating cube is an effect where a cube spins, showing family photos on its sides.",link:"https://vennilavennila-6404-8443.zcodeusers.in/Assignment022/cube.html"}
+        {image:"breezy.png",name:"Breezy.hr",description:"Breezy.hr is a website for hiring and managing job applications.",link:"https://vennilavennila-6404-8443.zcodeusers.in/htmlproject/main.html"},
+        {image:"vortex.jpg",description:"Vortex by Pixelarity",name:"Vortex",link:"https://vennilavennila-6404-8443.zcodeusers.in/Assignment015/page1.html"},
+        {image:"magnetic.jpg",description:"Magnetic by Pixelarity",name:"Magnetic",link:"https://vennilavennila-6404-8443.zcodeusers.in/Assignment010/magnetic.html"},
+        {image:"cascade.jpg",description:"Cascade by Pixelarity",name:"Cascade",link:"https://vennilavennila-6404-8443.zcodeusers.in/Assignment018/page2.html"},
+        {image:"hyperspace.jpg",description:"Hyperspace is a sleek and modern website template designed for startups and businesses.",name:"Hyperspace",link:"https://vennilavennila-6404-8443.zcodeusers.in/Assignment019/page1.html"},
+        {image:"3d.png",description:"A 3D rotating cube is an effect where a cube spins, showing family photos on its sides.",name:"Cube",link:"https://vennilavennila-6404-8443.zcodeusers.in/Assignment022/cube.html"}
     ]
 
     return(
         <div id="otherProjects">
             {
                 skill=="js" ? jsProject.map((data)=>{
-                    return(<OtherProject image={data.image} description={data.description} link={data.link}/>)
+                    return(<OtherProject image={data.image} name={data.name} description={data.description} link={data.link}/>)
                 }) : skill=="java" ? javaProject.map((data)=>{
-                    return(<OtherProject image={data.image} description={data.description} link={data.link}/>)
+                    return(<OtherProject image={data.image} name={data.name} description={data.description} link={data.link}/>)
                 }) : skill=="html" ? htmlProject.map((data)=>{
-                    return(<OtherProject image={data.image} description={data.description} link={data.link}/>)
+                    return(<OtherProject image={data.image} name={data.name} description={data.description} link={data.link}/>)
                 }) : skill=="scratch" ? scratchProject.map((data)=>{
-                    return(<OtherProject image={data.image} description={data.description} link={data.link}/>)
+                    return(<OtherProject image={data.image} name={data.name} description={data.description} link={data.link}/>)
                 }) : reactProject.map((data)=>{
-                    return(<OtherProject image={data.image} description={data.description} link={data.link}/>)
+                    return(<OtherProject image={data.image} name={data.name} description={data.description} link={data.link}/>)
                 }) 
             }
         </div>
