002d3d2b84b7d2d044c1b0a770a8351bdaf6f6a4
Vennila384 <vennik447@gmail.com>
2025-03-19 04:38:34 +0530

    alignment

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -13,7 +13,7 @@ export default function Skills()
     const skill2=[
         {src:"mysql.png",language:"Mysql",background:"rgba(68, 121, 161, 0.1)",border:"1px solid rgb(68, 121, 161)",color:"rgb(68, 121, 161)"},
         {src:"html.png",language:"HTML/CSS",background:"rgba(228, 77, 38, 0.1)",border: "1px solid rgb(228, 77, 38)",color:"rgb(228, 77, 38)"},
-        {src:"linux_logo.png",language:"Linux Command",background:"rgba(240, 219, 79, 0.1)",border: "1px solid rgb(240, 219, 79)",color:"rgb(240, 219, 79)"},
+        // {src:"linux_logo.png",language:"Linux Command",background:"rgba(240, 219, 79, 0.1)",border: "1px solid rgb(240, 219, 79)",color:"rgb(240, 219, 79)"},
         {src:"scratch.png",language:"Scratch",background:"rgba(77, 179, 61, 0.1)",border:"1px solid rgb(77, 179, 61)",color:"rgb(77, 179, 61)"},
         // {src:"",language:"",background:"",border:"",color:""}
     ]
@@ -21,14 +21,14 @@ export default function Skills()
     return(
         <div id="skills">
             <h1 style={{fontSize:"49px",color:"white"}}>Skills</h1>
-            <div className="skillDiv">
+            <div className="skillDiv1">
                 {
                     skill1.map((skill)=>{
                         return(<Skill src={skill.src} language={skill.language} background={skill.background} border={skill.border} color={skill.color}/>)
                     })
                 }
             </div>
-            <div  className="skillDiv">
+            <div  className="skillDiv2">
                 {
                     skill2.map((skill)=>{
                         return(<Skill src={skill.src} language={skill.language} background={skill.background} border={skill.border} color={skill.color}/>)
